9eb71aeeb3adffee59942e5d318b5058a69d0a77
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-09-30 21:04:17 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = false) {
+  if (array.length === 0 || !array) {
+    return 0;
+  }
+  const min = array.reduce((min, item) => item < min ? item : min);
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = false) {
+  if (array.length === 0 || !array) {
+    return 0;
+  }
+  const max = array.reduce((max, item) => item > max ? item : max);
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = false) {
+  if (array.length === 0 || !array) {
+    return 0;
+  }
+  const sum = array.reduce((sum, item) => sum + item);
+  const average = sum / array.length
+  return average;
 }
